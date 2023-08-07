b70da29d9e75fe6f5a40426824008831f052ae2f
wittn-git <wittner.dominic@gmail.com>
2023-08-07 21:53:31 +0200

    made experiment analysis folder

---
+++ experiments/scripts/AnalyzeData.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from tabulate import tabulate
+
+constrained = True
+
+csv_file, parameters_combinations, res_file = "", [], ""
+if constrained:
+    csv_file = "../data/test_mu1_constrained.csv"
+    parameters_combinations = ['mu', 'n', 'm', 'alpha']
+    res_file = "res_constrained.txt"
+else:
+    csv_file = "../data/test_mu1_unconstrained.csv"
+    parameters_combinations = ['mu', 'n', 'm']
+    res_file = "res_unconstrained.txt"
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+df = pd.read_csv(csv_file)
+
+grouped = df.groupby(parameters_combinations)
+occurrences = grouped.size().reset_index(name='occurrences')
+summary = grouped.agg({
+    'generations': 'mean',
+    'fitness': 'mean',
+    'opt': 'mean',
+    'diversity': 'mean',
+    'max_generations': 'mean'
+}).reset_index()
+
+summary['fitness_worse_than_opt'] = grouped.apply(lambda x: (x['fitness'] < x['opt']).sum()).reset_index(drop=True)
+std_generations = grouped['generations'].std().reset_index(name='std_generations')
+
+result = pd.merge(occurrences, summary, on=parameters_combinations)
+result = pd.merge(result, std_generations, on=parameters_combinations)
+
+result = result.round(2)
+
+with open(res_file, 'w') as f:
+    f.write(tabulate(result, headers='keys', tablefmt='psql'))
---
+++ experiments/scripts/DatapointInformation.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+constrained = False
+
+csv_file, parameters_combinations = "", []
+if constrained:
+    csv_file = "../data/test_mu1_constrained.csv"
+    parameters_combinations = ['mu', 'n', 'm', 'alpha']
+else:
+    csv_file = "../data/test_mu1_unconstrained.csv"
+    parameters_combinations = ['mu', 'n', 'm']
+
+df = pd.read_csv(csv_file)
+
+num_datapoints = len(df)
+num_duplicates = num_datapoints - len(df.drop_duplicates())
+value_combinations = df.groupby(parameters_combinations).size().reset_index(name='occurrences')
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+print("Number of datapoints:", num_datapoints)
+print("Number of duplicate datapoints:", num_duplicates)
+print("Possible value combinations:\n", value_combinations)
