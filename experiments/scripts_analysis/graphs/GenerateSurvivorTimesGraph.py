75bb8e512abb92a59a206d80fc12d0881ac5a923
wittn-git <wittner.dominic@gmail.com>
2023-09-13 11:25:37 +0200

    surivir opt graph

--- experiments/scripts_analysis/graphs/GenerateSurvivorTimesGraph.py
+++ experiments/scripts_analysis/graphs/GenerateSurvivorTimesGraph.py
@@ -3,25 +3,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def make_plot(outputfile):
+
     df = pd.read_csv('../../data/data_survivor-opt.csv')
+    df['runtime'] = df['runtime'] / df['generations']
     grouped = df.groupby(['mu', 'n', 'm', 'type'])['runtime'].mean().reset_index()
     unique_groups = grouped[['mu', 'n', 'm']].drop_duplicates().values
 
-    custom_colors = {'selectParents': "#B0E0E6", 'mutate': "#C71585"}
-    # TODO finish
-
-    ax = grouped.pivot(index=['mu', 'n', 'm'], columns="component", values='time').plot(kind='bar', alpha=0.65, color=[custom_colors.get(c, 'gray') for c in grouped['component']])
-    
+    custom_colors = {'optimized': "#B0E0E6", 'unoptimized': "#C71585"}
     x_labels = ['(µ={}, n={}, m={})'.format(mu, n, m) for mu, n, m in unique_groups]
-    ax.set_xticks(range(len(x_labels)))
-    ax.set_xticklabels(x_labels)
 
-    ax.set_ylabel('Computation Time (µs)')
+    ax = grouped.pivot(index=['mu', 'n', 'm'], columns="type", values='runtime').plot(kind='bar', alpha=0.65, color=[custom_colors.get(c, 'gray') for c in grouped['type']])
+    
+    ax.set_ylabel('Computation Time (µs) per Generation')
     ax.set_xlabel('Parameter Combinations')
+    ax.set_xticks(range(len(x_labels)))
+    ax.set_xticklabels(x_labels, rotation=0)
+    ax.tick_params(axis='y')
 
-    plt.xticks(rotation=0)
-    plt.legend(title='Component', loc='upper left')
-    plt.tight_layout()
+    plt.gca().get_legend().set_title('')
+    plt.gcf().set_size_inches(8, 5)
+
     plt.savefig(outputfile)
 
 if __name__ == "__main__":
