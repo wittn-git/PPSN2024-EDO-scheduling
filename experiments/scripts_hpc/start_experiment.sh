8bbe015e69ab05be0381312c37f5d56e70ce773c
wittn-git <wittner.dominic@gmail.com>
2023-09-17 20:44:27 +0200

    updated traj. scripts

--- experiments/scripts_hpc/start_experiment.sh
+++ experiments/scripts_hpc/start_experiment.sh
@@ -3,10 +3,8 @@
 start_job() {
     JOB_NAME=$2_$3_a${8//./}_l${9//./}
     OUTPUT_FILE=output_$JOB_NAME.csv
-    sed "s/JOB_NAME/$JOB_NAME/g; s/WALL_TIME/$1/g" job_template.sh > temp_job.sh
-    sbatch temp_job.sh $2 $3 $OUTPUT_FILE $4 $5 $6 $7 $8 $9
-    sleep 10
-    rm temp_job.sh
+    sh build_run.sh $2 $3 $OUTPUT_FILE $4 $5 $6 $7 $8 $9 &
+    sleep 2
 }
 
 if [ "$#" -ne 9 ]; then
