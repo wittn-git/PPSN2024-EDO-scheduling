d10780665630910eae9407f0802ad3724fc46168
wittn-git <wittner.dominic@gmail.com>
2023-09-28 15:47:41 +0200

    removed noah from main

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 /*
     Parameters (in order):
-        - Algorithm: {"Mu1-const", "Mu1-unconst", "NOAH"}
+        - Algorithm: {"Mu1-const", "Mu1-unconst"}
         - Mutation-Operator: {"1RAI", "XRAI", "NSWAP"}
         - Output-File: String
         - runs: Int
@@ -20,8 +20,6 @@ int main(int argc, char **argv){
 
     if(experiment_type == "Mu1-const" || experiment_type == "Mu1-unconst"){
         test_algorithm(mus, ns, ms, alphas, runs, output_file, experiment_type, operator_string, mutation_operator);
-    }else if(experiment_type == "NOAH"){
-        test_noah(mus, ns, ms, runs, 1, 0.5, 0.5, output_file, operator_string, mutation_operator);
     }else{
         throw std::invalid_argument("Invalid experiment type.");
     }
