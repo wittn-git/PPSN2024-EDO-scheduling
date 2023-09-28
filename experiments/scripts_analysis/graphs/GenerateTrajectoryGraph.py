ba3c7a758db214da2ea8e4b0d4046b6f98900251
wittn-git <wittner.dominic@gmail.com>
2023-09-28 14:59:08 +0200

    Added mutation operators to grouping of trajectory graph

--- experiments/scripts_analysis/graphs/GenerateTrajectoryGraph.py
+++ experiments/scripts_analysis/graphs/GenerateTrajectoryGraph.py
@@ -4,34 +4,34 @@ import matplotlib.pyplot as plt
 
 def make_plot(input_file, output_file, constrained):
     df = pd.read_csv(input_file)
-    grouped = df.groupby(['mu', 'n'])
-    for (mu, n), group in grouped:
+    grouped = df.groupby(['mu', 'n', 'run'])
+    for (mu, n, run), group in grouped:
         fig, ax = plt.subplots()
 
         if(constrained):
-            # Group the data within this subgroup by 'alpha'
-            alpha_groups = group.groupby(['alpha', 'm'])
-            for (alpha, m), alpha_group in alpha_groups:
-                ax.plot(alpha_group['generations'], alpha_group['diversity'], label=f'Alpha {alpha}, m {m}')
+            alpha_groups = group.groupby(['alpha', 'm', 'mutation'])
+            for (alpha, m, operator), alpha_group in alpha_groups:
+                ax.plot(alpha_group['generations'], alpha_group['diversity'], label=f'Alpha {alpha}, m {m}, operator {operator}')
         else:
-            m_groups = group.groupby(['m'])
-            for m, m_group in m_groups:
-                ax.plot(m_group['generations'], m_group['diversity'], label=f'm {m}')       
+            m_groups = group.groupby(['m', 'mutation'])
+            for (m, operator), m_group in m_groups:
+                ax.plot(m_group['generations'], m_group['diversity'], label=f'm {m}, operator {operator}')       
 
         ax.set_xlabel('Generation')
         ax.set_ylabel('Diversity')
         ax.set_title(f'µ={mu}, n={n}')
         ax.legend()
 
-        plt.savefig(f'{output_file}_mu_{mu}_n_{n}_plot.png')
+        plt.savefig(f'{output_file}_mu_{mu}_n_{n}_run_{run}_plot.png')
+        plt.close()
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python3 GenerateComponenttimesGraph.py <output_file> <constrained>")
+        print("Usage: python3 GenerateTrajectoryGraph.py <output_file> <constrained>")
         exit(1)
     
     output_file = sys.argv[1]
     constrained = sys.argv[2] == 'True'
-    input_file = f"../../data/trajectory_experiments/data_mu1-{'' if constrained else 'un'}const_trajectory_summarized.csv"
+    input_file = f"../../../../../Documents/trajectory_experiments/data_mu1-{'' if constrained else 'un'}const_trajectory.csv"
 
     make_plot(input_file, output_file, constrained)
